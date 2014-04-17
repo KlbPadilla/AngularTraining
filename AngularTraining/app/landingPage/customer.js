a60bb62118debb2d7b20316d3e19d847ea09027e
OscarAgreda <oscar1027@yahoo.com>
2014-04-17 17:07:38 -0600

    added comments when facebook starts

--- AngularTraining/app/landingPage/customer.js
+++ AngularTraining/app/landingPage/customer.js
@@ -3,21 +3,17 @@
     var controllerId = 'customer';
     angular.module('app').controller(controllerId, ['$scope', '$modalInstance', 'applicationData', customer]);
     function customer($scope, $modalInstance, applicationData) {
-        var c = applicationData.getDataItem(); //currentCustomer
 
-
-
+        var c = applicationData.getDataItem();
         $scope.c = c;
 
         $scope.ok = function () {
-
             $modalInstance.close($scope.c);
-
         };
+
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
-
 
     }
 })();
